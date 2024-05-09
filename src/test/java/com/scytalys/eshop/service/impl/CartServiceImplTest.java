810b8125a7df2d9a7aacd80f39dceb25404d43e2
dimitris <iracleous@gmail.com>
2024-05-09 13:39:08 +0300

    Testing

--- src/test/java/com/scytalys/eshop/service/impl/CartServiceImplTest.java
+++ src/test/java/com/scytalys/eshop/service/impl/CartServiceImplTest.java
@@ -1,16 +1,13 @@
 package com.scytalys.eshop.service.impl;
 
 import com.scytalys.eshop.dto.ResponseApi;
-import com.scytalys.eshop.mapper.EshopMapper;
 import com.scytalys.eshop.model.Customer;
-import com.scytalys.eshop.repository.CartProductRepository;
-import com.scytalys.eshop.repository.CartRepository;
+import com.scytalys.eshop.model.Employee;
 import com.scytalys.eshop.repository.CustomerRepository;
-import com.scytalys.eshop.repository.ProductRepository;
-import com.scytalys.eshop.service.CartService;
+
 import com.scytalys.eshop.service.CustomerValidator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
@@ -25,13 +22,7 @@ class CartServiceImplTest {
     private Customer customer;
 
     @Mock
-    private  CartRepository cartRepository;
-    @Mock
     private  CustomerRepository customerRepository;
-    @Mock
-    private  ProductRepository productRepository;
-    @Mock
-    private  CartProductRepository cartproductRepository;
     @Spy
     private  CustomerValidator customerValidator = new  CustomerValidatorImpl() ;
 
@@ -42,16 +33,17 @@ class CartServiceImplTest {
     void getCustomer() {
     }
 
-    @Test
-    void createCustomer() {
+    @BeforeEach
+    void setUp() {
         customer = new Customer();
         customer.setEmail("email@email.com");
         customer.setZip("11111");
         customer.setName("name");
+    }
 
-        when(customerRepository.save(customer))
-                .thenReturn(customer);
-
+    @Test
+    void createCustomer() {
+        when(customerRepository.save(customer)) .thenReturn(customer);
         ResponseApi<Customer> customerResponseApi = cartService.createCustomer(customer);
 
         assertNotNull(customerResponseApi);
